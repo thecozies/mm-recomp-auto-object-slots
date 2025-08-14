63b962b8e9147f2b5e8dfcd875bb64407e9776af
thecozies <79979276+thecozies@users.noreply.github.com>
2025-08-14 13:18:51 -0500

    shared stack for object ids and disable check for auto_slot_loading_enabled

--- src/auto_slots.c
+++ src/auto_slots.c
@@ -27,12 +27,78 @@ typedef struct {
 IdSlots all_id_slots[ACTOR_ID_MAX];
 GlobalSlots global_slots;
 
+bool auto_slot_loading_enabled = false;
+
 // Tracks how many layers of recursive slot loading are active. This is needed because actors can spawn other actors,
 // which in turn loads the child actor ID's slot set.
 // This tracking allows the mod to reload the parent actor's slot set when the child actor's spawning is finished.
 #define SLOT_SET_STACK_SIZE 64
-u32 slot_load_stack_depth = 0;
-ActorId slot_load_id_stack[SLOT_SET_STACK_SIZE];
+
+struct ActorIdStack {
+    PlayState* play;
+    ActorId ids[SLOT_SET_STACK_SIZE];
+    s32 depth;
+};
+
+void load_slots(PlayState* play, ActorId id);
+void unload_slots(PlayState* play, ActorId id);
+
+bool push_actor_stack(struct ActorIdStack *actor_stack, ActorId id, PlayState* play) {
+    if (actor_stack->depth < SLOT_SET_STACK_SIZE) {
+        actor_stack->ids[actor_stack->depth] = id;
+        actor_stack->play = play;
+        actor_stack->depth++;
+        return true;
+    } else {
+        recomp_printf("Warning: Actor ID stack overflow, max depth is %d\n", SLOT_SET_STACK_SIZE);
+    }
+    return false;
+}
+
+ActorId pop_actor_stack(struct ActorIdStack *actor_stack) {
+    if (actor_stack->depth > 0) {
+        actor_stack->depth--;
+        return actor_stack->ids[actor_stack->depth];
+    } else {
+        recomp_printf("Warning: Actor ID stack underflow\n");
+    }
+    return ACTOR_ID_MAX;
+}
+
+ActorId get_actor_stack_top(struct ActorIdStack *actor_stack) {
+    if (actor_stack->depth > 0) {
+        return actor_stack->ids[actor_stack->depth - 1];
+    }
+    recomp_printf("Warning: Actor ID stack is empty, returning ACTOR_ID_MAX\n");
+    return ACTOR_ID_MAX;
+}
+
+void on_push_to_actor_stack(struct ActorIdStack *actor_stack, ActorId id, PlayState* play) {
+    if (push_actor_stack(actor_stack, id, play)) {
+        auto_slot_loading_enabled = true;
+        load_slots(actor_stack->play, get_actor_stack_top(actor_stack));
+    }
+}
+
+void on_pop_from_actor_stack(struct ActorIdStack *actor_stack) {
+    ActorId popped_id = pop_actor_stack(actor_stack);
+    if (popped_id != ACTOR_ID_MAX) {
+        unload_slots(actor_stack->play, popped_id);
+        if (actor_stack->depth == 0) {
+            // If the stack is empty, reset the auto slot loading state.
+            actor_stack->play = NULL;
+            auto_slot_loading_enabled = false;
+        }
+    } else {
+        auto_slot_loading_enabled = false;
+    }
+}
+
+
+
+// u32 slot_load_stack_depth = 0;
+// ActorId slot_load_id_stack[SLOT_SET_STACK_SIZE];
+struct ActorIdStack slot_load_id_stack = {NULL, {0}, 0};
 
 void propagate_persistent_slots(ObjectContext* objectCtx) {
     recomp_printf("Copying %d persistent slots\n", objectCtx->numPersistentEntries);
@@ -74,7 +140,7 @@ void load_slots(PlayState* play, ActorId id) {
         // recomp_printf("Loading slots for ID 0x%04X\n", id);
 
         // If there is no alternate slot set already in use, save the current object context slots as the global set.
-        if (slot_load_stack_depth == 0) {
+        if (slot_load_id_stack.depth == 1) {
             global_slots.numEntries = play->objectCtx.numEntries;
             global_slots.numPersistentEntries = play->objectCtx.numPersistentEntries;
             global_slots.mainKeepSlot = play->objectCtx.mainKeepSlot;
@@ -88,10 +154,6 @@ void load_slots(PlayState* play, ActorId id) {
 
         // Load the slot set for the given actor ID.
         load_slots_impl(&play->objectCtx, id);
-
-        // Increment the slot stack depth.
-        slot_load_id_stack[slot_load_stack_depth] = id;
-        slot_load_stack_depth++;
     }
 }
 
@@ -108,7 +170,7 @@ void unload_slots(PlayState* play, ActorId id) {
         cur_id_slots->numEntries = play->objectCtx.numEntries;
 
         // If this is the parent-most actor in the chain, reload the global slot set into the object context.
-        if (slot_load_stack_depth == 1) {
+        if (slot_load_id_stack.depth == 0) {
             // Restore the global context.
             play->objectCtx.numEntries = global_slots.numEntries;
             play->objectCtx.numPersistentEntries = global_slots.numPersistentEntries;
@@ -122,10 +184,8 @@ void unload_slots(PlayState* play, ActorId id) {
         }
         // Otherwise, load the parent actor's slot set.
         else {
-            load_slots_impl(&play->objectCtx, slot_load_id_stack[slot_load_stack_depth - 1]);
+            load_slots_impl(&play->objectCtx, get_actor_stack_top(&slot_load_id_stack));
         }
-
-        slot_load_stack_depth--;
     }
 }
 
@@ -139,58 +199,76 @@ typedef struct {
     /* 0x18 */ u32 updateActorFlagsMask; // Actor will update only if at least 1 actor flag is set in this bitmask
 } UpdateActor_Params;
 
-bool auto_slot_loading_enabled = false;
-PlayState* current_spawn_play = NULL;
-ActorId current_spawn_id = ACTOR_ID_MAX;
+const char *get_actor_define_string(ActorId id) {
+    static const char* actor_names[] = {
+        #define DEFINE_ACTOR_INTERNAL(_name, enumValue, _alloc, _str) #enumValue,
+        #define DEFINE_ACTOR(_name, enumValue, _alloc, _str) #enumValue,
+        #define DEFINE_ACTOR_UNSET(enumValue) #enumValue,
+        #include "tables/actor_table.h"
+    };
+
+    return id < ACTOR_ID_MAX ? actor_names[id] : "Unknown";
+    #undef DEFINE_ACTOR_INTERNAL
+    #undef DEFINE_ACTOR
+    #undef DEFINE_ACTOR_UNSET
+}
+
+
+// struct ActorIdStack current_spawn_stack = {
+//     .play = NULL,
+//     .ids = {0},
+//     .depth = 0
+// };
+#define current_spawn_stack slot_load_id_stack
+
+// ActorId current_spawn_id = ACTOR_ID_MAX;
 RECOMP_HOOK("Actor_SpawnAsChildAndCutscene") void on_spawn(ActorContext* actorCtx, PlayState* play, s16 index, f32 x, f32 y, f32 z, s16 rotX,
                                      s16 rotY, s16 rotZ, s32 params, u32 csId, u32 halfDaysBits, Actor* parent)
 {
-    current_spawn_id = index;
-    current_spawn_play = play;
-    auto_slot_loading_enabled = true;
-    load_slots(current_spawn_play, current_spawn_id);
+    on_push_to_actor_stack(&current_spawn_stack, index, play);
+    if (parent != NULL) {
+        recomp_printf("Spawning child of %20s (ID: 0x%04X)\n    ",
+                     get_actor_define_string(parent->id), parent->id);
+    }
+    recomp_printf("Spawning actor %20s (ID: 0x%04X) stack_depth: %2d\n",
+                 get_actor_define_string(index), index, current_spawn_stack.depth);
 }
 
 RECOMP_HOOK_RETURN("Actor_SpawnAsChildAndCutscene") void after_spawn() {
-    unload_slots(current_spawn_play, current_spawn_id);
-    current_spawn_id = ACTOR_ID_MAX;
-    current_spawn_play = NULL;
-    auto_slot_loading_enabled = false;
+    on_pop_from_actor_stack(&current_spawn_stack);
 }
 
-PlayState* current_draw_play = NULL;
-ActorId current_draw_id = ACTOR_ID_MAX;
+
+// struct ActorIdStack current_draw_stack = {
+//     .play = NULL,
+//     .ids = {0},
+//     .depth = 0
+// };
+#define current_draw_stack current_spawn_stack
+
 RECOMP_HOOK("Actor_Draw") void on_draw(PlayState* play, Actor* actor) {
-    current_draw_id = actor->id;
-    current_draw_play = play;
-    auto_slot_loading_enabled = true;
-    load_slots(current_draw_play, current_draw_id);
+    on_push_to_actor_stack(&current_draw_stack, actor->id, play);
 }
 
 RECOMP_HOOK_RETURN("Actor_Draw") void after_draw() {
-    unload_slots(current_draw_play, current_draw_id);
-    current_draw_id = ACTOR_ID_MAX;
-    current_draw_play = NULL;
-    auto_slot_loading_enabled = false;
+    on_pop_from_actor_stack(&current_draw_stack);
 }
 
-PlayState* current_update_play = NULL;
-ActorId current_update_id = ACTOR_ID_MAX;
+// struct ActorIdStack current_update_stack = {
+//     .play = NULL,
+//     .ids = {0},
+//     .depth = 0
+// };
+#define current_update_stack current_spawn_stack
+
 RECOMP_HOOK("Actor_UpdateActor") void on_update(UpdateActor_Params* params) {
     PlayState* play = params->play;
     Actor* actor = params->actor;
-
-    current_update_id = actor->id;
-    current_update_play = play;
-    auto_slot_loading_enabled = true;
-    load_slots(current_update_play, current_update_id);
+    on_push_to_actor_stack(&current_update_stack, actor->id, play);
 }
 
 RECOMP_HOOK_RETURN("Actor_UpdateActor") void after_update() {
-    unload_slots(current_update_play, current_update_id);
-    current_update_id = ACTOR_ID_MAX;
-    current_update_play = NULL;
-    auto_slot_loading_enabled = false;
+    on_pop_from_actor_stack(&current_update_stack);
 }
 
 void print_context(ObjectContext* objectCtx) {
@@ -198,6 +276,20 @@ void print_context(ObjectContext* objectCtx) {
     for (int i = 0; i < OBJECT_SLOT_COUNT; i++) {
         recomp_printf("  id %04X, seg %08X\n", objectCtx->slots[i].id, (u32)objectCtx->slots[i].segment);
     }
+}
+
+const char *get_obj_define_string(s16 objectId) {
+    static const char* object_names[] = {
+        #define DEFINE_OBJECT(_name, enumValue) #enumValue,
+        #define DEFINE_OBJECT_UNSET(enumValue) #enumValue,
+        #define DEFINE_OBJECT_EMPTY(_name, enumValue) #enumValue,
+        #include "tables/object_table.h"
+    };
+
+    return objectId < OBJECT_ID_MAX ? object_names[objectId] : "Unknown";
+    #undef DEFINE_OBJECT
+    #undef DEFINE_OBJECT_UNSET
+    #undef DEFINE_OBJECT_EMPTY
 }
 
 // Patched to load objects if the slot wasn't found and a free space exists.
@@ -213,17 +305,17 @@ RECOMP_PATCH s32 Object_GetSlot(ObjectContext* objectCtx, s16 objectId) {
     }
 
     // @mod Search for an empty slot and load the object if auto slot loading is currently enabled.
-    if (auto_slot_loading_enabled) {
+    // if (auto_slot_loading_enabled) {
         if (objectCtx->numEntries < OBJECT_SLOT_COUNT) {
             int slot = objectCtx->numEntries;
-            recomp_printf("Auto loading object 0x%04X into slot %d\n", objectId, i);
+            recomp_printf("Auto loading object %24s 0x%04X into slot %d\n", get_obj_define_string(objectId), objectId, i);
             objectCtx->numEntries++;
             objectCtx->slots[slot].id = objectId;
             objectCtx->slots[slot].segment = GlobalObjects_getGlobalObject(objectId);
             // print_context(objectCtx);
             return slot;
         }
-    }
+    // }
 
     // recomp_printf("  Not found\n");
     return OBJECT_SLOT_NONE;
@@ -236,24 +328,4 @@ RECOMP_PATCH void* func_8012F73C(ObjectContext* objectCtx, s32 slot, s16 id) {
     objectCtx->slots[slot].segment = GlobalObjects_getGlobalObject(id);
 
     return NULL;
-}
-
-// Spawn redead at link if pressing L.
-RECOMP_HOOK("Player_Update") void on_player_update(Actor* thisx, PlayState* play) {
-    Player* player = GET_PLAYER(play);
-    if (player == NULL) {
-        return;
-    }
-
-    if (play->state.input[0].press.button & L_TRIG) {
-        for (int i = 0; i < 8; i++) {
-            u32 angle = (0x10000U / 8) * i;
-            const float spawnDist = 20.0f;
-            float offsetX = Math_SinS(angle) * spawnDist;
-            float offsetZ = Math_CosS(angle) * spawnDist;
-            Actor_Spawn(&play->actorCtx, play, ACTOR_EN_RD, player->actor.world.pos.x + offsetX, player->actor.world.pos.y, player->actor.world.pos.z + offsetZ,
-                0, angle + 0x8000, 0,
-                0x7F07);
-        }
-    }
 }
